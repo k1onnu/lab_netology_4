cd236f179638d3a5fdc617a7a6980ba594dacf59
k1 <tichdies@gmail.com>
2024-11-14 20:25:23 +0300

    Converted json to xml

--- Convert.py
+++ Convert.py
@@ -1,20 +1,32 @@
 import json
 import xml.etree.ElementTree as ET
+from xml.dom import minidom
 
 def json_to_xml(json_data):
-
+    # Создаем корневой элемент
     root = ET.Element("People")
+
+    # Заполняем XML-элементами на основе данных JSON
     for person in json_data:
         person_element = ET.SubElement(root, "Person")
         for key, value in person.items():
             pers = ET.SubElement(person_element, key)
             pers.text = str(value)
-    return ET.tostring(root, encoding='utf-8').decode('utf-8')
 
+    # Возвращаем строку XML, отформатированную в многострочном виде
+    xml_str = ET.tostring(root, encoding='utf-8')
+    parsed_xml = minidom.parseString(xml_str)
+    return parsed_xml.toprettyxml(indent="    ")
+
+
+# Чтение данных из JSON-файла
 with open('data.json', 'r', encoding='utf-8') as json_file:
     data = json.load(json_file)
 
+#Конвертируем JSON to XML
 xml_data = json_to_xml(data)
+
+# Запись отформатированного XML в файл
 with open('data.xml', 'w', encoding='utf-8') as xml_file:
     xml_file.write(xml_data)
 
